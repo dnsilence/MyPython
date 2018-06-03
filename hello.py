f5cdcc732e002d8765955b98beab3b1ebf009915
DN_Silence <DN_Silence@hotmail.com>
2018-06-03 16:26:57 +0800

    Initial commit

---
+++ hello.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+print('The quick brown fox', 'jumps over', 'the lazy dog')
+print(300)
+print('100 + 200 =', 100 + 200)
+name = input('please enter your name: ')
+print('hello,', name)
+print('1024 * 768 =', 1024 * 768)
