3f6f143cfe95197289494d5b1c1bcd734af0dc61
DN_Silence <DN_Silence@hotmail.com>
2018-06-03 16:43:07 +0800

    first class

--- hello.py
+++ hello.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
-print('The quick brown fox', 'jumps over', 'the lazy dog')
-print(300)
-print('100 + 200 =', 100 + 200)
-name = input('please enter your name: ')
-print('hello,', name)
-print('1024 * 768 =', 1024 * 768)
+# print('The quick brown fox', 'jumps over', 'the lazy dog')
+#
+# print(300)
+# print('100 + 200 =', 100 + 200)
+#
+# name = input('please enter your name: ')
+# print('hello,', name)
+#
+# print('1024 * 768 =', 1024 * 768)
+
+print('I\'m ok.')
+print('I\'m learning\nPython.')
+print('\\\n\\')
+print('\\\t\\')
+print(r'\\\t\\')
+print('''line1
+line2
+line3''')
+print(r'''hello,\n
+world''')
